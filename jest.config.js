0dd79431c831d661abb93e09452dc7c475769261
Koen Verburg <creativekoen@gmail.com>
2021-02-02 22:27:30 +0100

    tests(scss file) added identity-obj-proxy

--- jest.config.js
+++ jest.config.js
@@ -38,7 +38,7 @@ module.exports = {
 
   moduleNameMapper: {
     // '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/__mocks__/static.mock.ts',
-    // '\\.(css|sass|scss|styl|less)$': '<rootDir>/config/__mocks__/style.mock.ts',
+    '\\.(css|sass|scss|styl|less)$': 'identity-obj-proxy',
     '@utils': '<rootDir>/src/utils',
     '@hoc': '<rootDir>/src/hoc',
     '@generic': '<rootDir>/src/generic',
