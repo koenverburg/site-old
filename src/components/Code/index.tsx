fdf9b4c3191c758836835171a4628dd9e6e36aa4
Koen Verburg <creativekoen@gmail.com>
2021-02-03 16:28:38 +0100

    feat(code renderer) updated the colorscheme

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -1,5 +1,5 @@
 import Highlight, {defaultProps} from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/nightOwl'
+import theme from 'prism-react-renderer/themes/palenight'
 import styles from './code.module.scss'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
