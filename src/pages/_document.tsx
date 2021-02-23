f63cc0d36a6ede65f59c6fb8ee456ff9e711dd2d
Koen Verburg <creativekoen@gmail.com>
2021-02-23 18:04:49 +0100

    feat(seo) update the layout to have a higher seo score with meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Document, {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps} from 'next/document'
-import {data} from 'data/static'
 
 class MyDocument extends Document {
   public static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -17,27 +16,11 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#f8f8f8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
-          <meta name="description" content={data.description} />
-          <meta name="keywords" content={data.keywords.join(',')} />
-          <meta name="sitemap" content={`${data.urls.https}/sitemap.xml`} />
-
-          <meta name="twitter:site" content={data.urls.https} />
-          <meta name="twitter:card" key="twcard" content="summary_large_image" />
-          <meta name="twitter:creator" key="twhandle" content={data.handles.twitter.handle} />
-
-          <meta property="og:title" key="ogtitle" content={data.name} />
-          <meta property="og:url" key="ogurl" content={data.urls.https} />
-          <meta property="og:site_name" key="ogsitename" content={data.name} />
-          <meta property="og:description" key="ogdesc" content={data.description} />
-          <meta property="og:image" key="ogimage" content="/images/avatar.jpg" />
-          <meta property="og:locale" content="nl_NL" />
-          <meta property="og:type" content="website" />
-
-          <link rel="manifest" href="/manifest.json" />
+          <meta name="sitemap" content='/sitemap.xml' />
           <link rel="author" type="text/plain" href="/humans.txt" />
 
           <meta name="mobile-web-app-capable" content="false" />
-          <meta name="apple-mobile-web-app-title" content={data.name} />
+          <meta name="apple-mobile-web-app-title" content="Koen Verburg" />
           <meta name="apple-mobile-web-app-capable" content="false" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
