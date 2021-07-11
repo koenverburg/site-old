7a0900e5666f6b034cedafe0cbea6899ed825b5f
Koen Verburg <creativekoen@gmail.com>
2021-07-11 11:26:16 +0200

    refactor: moved to jsx.element as return type and got rid of react.fc

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,7 +1,7 @@
 import NextImage from 'next/image'
 import styles from './image.module.scss'
 
-export const Image = (image: HTMLImageElement): React.ReactNode => {
+export const Image = (image: HTMLImageElement): JSX.Element => {
   return (
     <div className={styles.wrapperImage}>
       <NextImage className={styles.image} src={image.src} alt={image.alt} height="400" width="555" />
