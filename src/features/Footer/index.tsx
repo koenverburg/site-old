0e06cc1f248d09a0d563e7ec2b708d05394bbba4
Koen Verburg <creativekoen@gmail.com>
2021-09-28 22:58:22 +0200

    refactor: new footer layoutz

--- src/features/Footer/index.tsx
+++ src/features/Footer/index.tsx
@@ -12,26 +12,26 @@ const Footer = ({socials}: Props): JSX.Element => {
   return (
     <div className={cx('flex flex-col justify-center items-start pb-8')}>
       <hr className={cx('w-full border-1 border-gray-200 dark:border-gray-800 mb-4')}/>
-      <div className={cx('w-full grid grid-cols-3 gap-4')}>
+      <div className={cx('w-full')}>
         {socials &&
           <>
-            <ul className={cx('block space-y-4')}>
-              {socials.slice(0, 2).map(links => (
-                <li key={links.id} className={cx('text-gray-500 hover:text-gray-900')}>
+            <ul className={cx('inline-flex space-x-4')}>
+              {socials.map(links => (
+                <li key={links.id} className={cx('flex-1 text-gray-500 hover:text-gray-900')}>
                   <a
                     role="button"
-                    aria-label={links.handle ?? 'social media link'}
-                    href={links.url ?? '#'}
-                    target="_blank"
                     rel="noopener noreferrer"
-                    className={cx('flex flex-row')}
+                    target="_blank"
+                    href={links.url ?? '#'}
+                    aria-label={links.handle ?? 'Social Media Link'}
                   >
                     <Icon name={links.icon} />
-                    <span className={cx('mx-1')}>{links.handle}</span>
                   </a>
                 </li>
               ))}
             </ul>
+            {/*<span className={cx('mx-1')}>{links.handle}</span>*/}
+            {/*
             <ul className={cx('block space-y-4')}>
               {socials.slice(2, 4).map(links => (
                 <li key={links.id} className={cx('text-gray-500 hover:text-gray-900')}>
@@ -66,10 +66,11 @@ const Footer = ({socials}: Props): JSX.Element => {
                 </li>
               ))}
             </ul>
+            */}
+            <span className={cx('align-top text-center text-gray-500 hover:text-gray-900')}>Copyright {(new Date().getFullYear())}</span>
           </>
         }
       </div>
-      <span className={cx('w-full text-center text-gray-500 hover:text-gray-900 mt-6')}>Copyright {(new Date().getFullYear())}</span>
     </div>
   )
 }
