2e42ec7cbdfd6a3b001eff04ef576576c3661889
Koen Verburg <creativekoen@gmail.com>
2021-09-30 07:57:49 +0200

    refactor: updated the home page and create a graphclient to use for every page

--- src/features/Footer/index.tsx
+++ src/features/Footer/index.tsx
@@ -12,9 +12,9 @@ const Footer = ({socials}: Props): JSX.Element => {
   return (
     <div className={cx('flex flex-col justify-center items-start pb-8')}>
       <hr className={cx('w-full border-1 border-gray-200 dark:border-gray-800 mb-4')}/>
-      <div className={cx('w-full')}>
+      <div className={cx('w-full sm:flex sm:justify-between')}>
         {socials &&
-          <>
+          <div className={cx('group flex justify-center mb-4 sm:mb-0')}>
             <ul className={cx('inline-flex space-x-4')}>
               {socials.map(links => (
                 <li key={links.id} className={cx('flex-1 text-gray-500 hover:text-gray-900')}>
@@ -30,46 +30,11 @@ const Footer = ({socials}: Props): JSX.Element => {
                 </li>
               ))}
             </ul>
-            {/*<span className={cx('mx-1')}>{links.handle}</span>*/}
-            {/*
-            <ul className={cx('block space-y-4')}>
-              {socials.slice(2, 4).map(links => (
-                <li key={links.id} className={cx('text-gray-500 hover:text-gray-900')}>
-                  <a
-                    role="button"
-                    aria-label={links.handle ?? 'social media link'}
-                    href={links.url ?? '#'}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('flex flex-row')}
-                  >
-                    <Icon name={links?.icon} />
-                    <span className={cx('mx-1')}>{links.handle}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-            <ul className={cx('block space-y-4')}>
-              {socials.slice(4, 6).map(links => (
-                <li key={links.id} className={cx('text-gray-500 hover:text-gray-900')}>
-                  <a
-                    role="button"
-                    aria-label={links.handle ?? 'social media link'}
-                    href={links.url ?? '#'}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('flex flex-row')}
-                  >
-                    <Icon name={links?.icon} />
-                    <span className={cx('mx-1')}>{links.handle}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-            */}
-            <span className={cx('align-top text-center text-gray-500 hover:text-gray-900')}>Copyright {(new Date().getFullYear())}</span>
-          </>
+          </div>
         }
+        <div className={cx('group text-center sm:text-right')}>
+          <span className={cx('align-top text-center text-gray-500 hover:text-gray-900')}>Copyright {(new Date().getFullYear())}</span>
+        </div>
       </div>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,99 @@
+import css from 'classnames'
+import React from 'react'
 import {gql} from '@apollo/client'
+import {Description, ProjectList} from '@components'
+import {MDXRemote} from 'next-mdx-remote'
+import {MarkdownComponents} from '@components'
+import {serialize} from 'next-mdx-remote/serialize'
+import {client} from 'lib/graphClient'
+import type {MDXResult} from '@types'
 
 type Props = {
   contentPage: any
+  content: MDXResult
+  seoDescription: string
 }
 
-const Index = (props: Props): JSX.Element => {
-  console.log(props.contentPage)
+const ProjectSection = ({section}) => {
   return (
-    <section className="section--hero">
-      <h1 className="hero">
-        Koen is a&nbsp;
-        <span>Developer</span>
-      </h1>
-    </section>
+    <div className={css('w-full max-w-none')}>
+      <h3 className={css('font-medium py-5')}>{section.title}</h3>
+      <ProjectList projects={section.projects} />
+    </div>
   )
 }
 
+const Index = ({contentPage, content, seoDescription}: Props): JSX.Element | null => {
+  return (
+    <>
+      <section>
+        <Description
+          title={'Home - Koen Verburg'}
+          description={seoDescription}
+        >
+        </Description>
+        <article className={css('prose w-full max-w-none')}>
+          <h2>{contentPage.title}</h2>
+          <MDXRemote
+            components={MarkdownComponents}
+            compiledSource={content.compiledSource}
+          />
+        </article>
+      </section>
+      {contentPage.sections && contentPage.sections.map(section =>
+        <ProjectSection key={section.id} section={section} />
+      )}
+    </>
+  )
+}
 
-export const HomePageFragment = {
-  content: gql`
-    fragment HomePage on Query {
-      contentPage(where: {id: "ckrjcvgxc33x30a553d8gcikp"}) {
+const query = gql`
+  query {
+    contentPage(where: {id: "ckrjcvgxc33x30a553d8gcikp"}) {
+      id
+      slug
+      title
+      description {
+        text
+        raw
+        markdown
+      }
+      sections {
         id
-        slug
         title
         description {
           markdown
         }
-        sections {
-          id
-          title
-          description {
-            markdown
-          }
-          projects {
-            ... on Project {
+        projects {
+          ... on Project {
+            id
+            link
+            projectName
+            tags {
+              slug
+              name
               id
-              link
-              projectName
-              tags {
-                slug
-                name
-                id
-              }
             }
           }
         }
       }
     }
-  `,
+  }
+`
+
+export async function getServerSideProps(context) {
+  const {contentPage} = await client.request(query)
+
+  const content = await serialize(contentPage.description.markdown ?? '')
+  const seoDescription = contentPage.description.text.replaceAll('\\n', '')
+
+  return {
+    props: {
+      content,
+      seoDescription,
+      contentPage,
+    },
+  }
 }
 
 export default Index
