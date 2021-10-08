8c0e5b56695900e429976b8aa2bace8532d00b20
Koen Verburg <creativekoen@gmail.com>
2021-10-08 16:37:40 +0200

    fix: resolved 500 error on vercel

--- src/lib/graphClient.ts
+++ src/lib/graphClient.ts
@@ -1,4 +1,4 @@
 import {GraphQLClient} from 'graphql-request'
 
-export const client = new GraphQLClient('http://localhost:3000/api/graphql')
+export const client = new GraphQLClient(`${process.env.VERCEL_URL}/api/graphql`)
 
