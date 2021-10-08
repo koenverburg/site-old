534d58b30fda07ac36dfc4fd898cc238d6abcf3f
Koen Verburg <creativekoen@gmail.com>
2021-10-08 15:52:51 +0200

    ci(build) places back the variables

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -3,7 +3,7 @@ import {GraphQLSchema, print} from 'graphql'
 import {ApolloServer} from 'apollo-server-micro'
 import {introspectSchema, wrapSchema} from '@graphql-tools/wrap'
 
-const executor = async ({document}) => {
+const executor = async ({document, variables}) => {
   const query = print(document)
   const fetchResult = await fetch(process.env.GRAPHCMS_URL, {
     method: 'POST',
@@ -11,14 +11,16 @@ const executor = async ({document}) => {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
     },
-    body: JSON.stringify({query}),
+    body: JSON.stringify({query, variables}),
   })
   return fetchResult.json()
-};
+}
 
 const createRemoteSchema = async (): Promise<GraphQLSchema> =>
   wrapSchema({
-    schema: await introspectSchema(executor, undefined),
+    // @ts-ignore // The typing of this function is off
+    schema: await introspectSchema(executor),
+    // @ts-ignore
     executor,
   })
 
