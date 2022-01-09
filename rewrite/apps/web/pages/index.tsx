bb04ca721d2d05df90cf6236b197c8e0db9ea91b
Koen Verburg <creativekoen@gmail.com>
2022-01-09 22:26:53 +0100

    chore: removed the button

--- rewrite/apps/web/pages/index.tsx
+++ rewrite/apps/web/pages/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
-import { Button } from '@packages/ui/components'
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button label='hii' />
     </div>
   )
 }
