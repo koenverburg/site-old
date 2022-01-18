0156806cc17f5c9ec05a00b76e9af4d3969bdc8c
Koen Verburg <creativekoen@gmail.com>
2022-01-18 22:27:32 +0100

    feat: create icon and divider component

---
+++ rewrite/packages/ui/components/divider/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export const Divider = () => {
+  return (
+    <hr className="w-full border-2 border-gray-200 dark:border-gray-800 mb-4" />
+  )
+}
--- rewrite/packages/ui/components/index.ts
+++ rewrite/packages/ui/components/index.ts
@@ -2,4 +2,6 @@ export { Button } from './button'
 export { Paragraph } from './paragraph'
 export { Grid } from './grid'
 export { GridItem } from './gridItem'
+export { Icon } from './icon'
+export { Divider } from './divider'
 
