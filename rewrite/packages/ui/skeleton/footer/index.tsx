62fdb4dcf9f642b372b44ee56379c2ae7f13254b
Koen Verburg <creativekoen@gmail.com>
2022-01-18 22:27:55 +0100

    improv: updated the footer with copyright and socials

--- rewrite/packages/ui/skeleton/footer/index.tsx
+++ rewrite/packages/ui/skeleton/footer/index.tsx
@@ -1,13 +1,44 @@
 import * as React from 'react'
+import cx from 'classnames'
+import { Social } from '../../types'
+import { Icon, Divider } from '../../components'
 
 export type Props = {
-  // children: React.ReactChild | React.ClassicElement
+  socials?: Social[]
 }
 
-export const Footer = (props: Props) => {
+export const Footer = ({ socials }: Props) => {
   return (
-    <>
-      {'I am footer'}
-    </>
+    <div className={cx('flex flex-col justify-center items-start pb-8')}>
+
+      <Divider />
+  
+      <div className={cx('w-full sm:flex sm:justify-between')}>
+        {socials &&
+          <div className={cx('group flex justify-center mb-4 sm:mb-0')}>
+            <ul className={cx('inline-flex space-x-4')}>
+              {socials.map(links => (
+                <li key={links.url} className={cx('flex-1 text-gray-500 hover:text-gray-900')}>
+                  <a
+                    role="button"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={links.url ?? '#'}
+                    aria-label={links.handle ?? 'Social Media Link'}
+                  >
+                    <Icon name={links.icon} />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        }
+
+        <div className={cx('group text-center sm:text-right')}>
+          <span className={cx('align-top text-center text-gray-500 hover:text-gray-900')}>Copyright {(new Date().getFullYear())}</span>
+        </div>
+
+      </div>
+    </div>
   )
 }
