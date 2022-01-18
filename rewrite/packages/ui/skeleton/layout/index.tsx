0bd7aa2e35209343805384551019221e09a36ddf
Koen Verburg <creativekoen@gmail.com>
2022-01-18 22:28:36 +0100

    improv: passed along socials to the footer component

--- rewrite/packages/ui/skeleton/layout/index.tsx
+++ rewrite/packages/ui/skeleton/layout/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Footer, Navigation, NavigationItem } from '..'
+import { Social } from '../../types'
 
 export type NavigationLinks = {
   to: string
@@ -8,12 +9,14 @@ export type NavigationLinks = {
 
 export type Props = {
   links?: NavigationLinks[]
+  socials?: Social[] 
   children: React.ReactNode
 }
 
 export const Layout = (props: Props) => {
   return (
     <div className="container mx-auto px-6 sm:px-3 md:px-0">
+
       <Navigation>
         {props.links && props.links.map(link => (
           <NavigationItem key={link.to} to={link.to} label={link.label} />
@@ -22,7 +25,7 @@ export const Layout = (props: Props) => {
 
       {props.children}
 
-      <Footer />
+      <Footer socials={props.socials}/>
     </div>
   )
 }
