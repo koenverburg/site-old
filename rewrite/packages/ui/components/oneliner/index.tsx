1a270d52a941d6b0f19bcaeb3a49bc6100e07379
Koen Verburg <creativekoen@gmail.com>
2022-04-03 13:50:26 +0200

    chore: random changes, kinda give up on what I was trying to do here

--- rewrite/packages/ui/components/oneliner/index.tsx
+++ rewrite/packages/ui/components/oneliner/index.tsx
@@ -2,38 +2,18 @@ import * as React from 'react'
 
 export type Props = {
   title: string
-  items?: string[]
   children?: React.ReactNode | React.ReactNode[]
 }
 
 export const Oneliner = ({
   title,
-  items
-}: Props) => {
-  if (!items) return null
-  const tail = items[items.length-1]
-
-  return (
+  children
+}: Props) =>
+  !children ? null : (
     <section className="w-max pb-8">
-
       <h3 className="text-base font-medium text-gray-900 mb-1">
         {title}{' '}
-
-        {items && items.map(item => (
-          <span key={item}>
-            <strong>{item}</strong>
-            {', '}
-          </span>
-        ))}
-
-        {tail && (
-          <>
-            {'and '}
-            <strong>{tail}</strong>
-          </>
-        )}
-
+        {children}
       </h3>
     </section>
   )
-}
