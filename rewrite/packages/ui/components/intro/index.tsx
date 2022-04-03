3043f3f2c94e26551d2b7ed234041fa3d9a74fc7
Koen Verburg <creativekoen@gmail.com>
2022-04-03 13:49:57 +0200

    chore: updated the structure for this component

--- rewrite/packages/ui/components/intro/index.tsx
+++ rewrite/packages/ui/components/intro/index.tsx
@@ -1,41 +1,86 @@
 import * as React from 'react'
+import { useTheme } from 'next-themes'
+import { Social } from '../../types'
+import { Icon } from '@packages/ui/components'
 
 export type Props = {
   firstName: string
   lastName: string
   position?: string
   companyName?: string
-  companyUrl?: string
   content?: string[]
+  avatarUrl?: string
+  socials?: Social[]
 }
 
 export const Intro = ({
   firstName,
   lastName,
-  position, 
-  companyUrl,
+  position,
   companyName,
-  content
+  content,
+  avatarUrl,
+  socials
 }: Props) => {
+  const [mounted, setMounted] = React.useState<boolean>(false)
+  const {theme, setTheme}= useTheme()
+  React.useEffect(() => setMounted(true), [])
+
   return (
-    <article className="text-neutral-900 pt-16 pb-8">
+    <div className="pt-8 lg:pt-16 pb-8">
+      <div className="flex items-center mb-4">
+        {avatarUrl && (
+          <div className="mr-4">
+            <div className="rounded-full bg-gradient-to-tl from-teal-200/60 to-yellow-400/60 shadow-lg p-1.5">
+              <div className="rounded-full h-[80px] w-[80px]">
+                <img className="rounded-full" src={avatarUrl} />
+              </div>
+            </div>
+          </div>
+        )}
 
-      <h1>
-        <span className="text-xl">
-          <span className="font-bold">{firstName}</span>{' '}<span>{lastName}</span>
-        </span>
-      </h1>
+        <div className="">
+          <h1 className="font-bold text-3xl tracking-tight">{firstName}{' '}{lastName}</h1>
 
-      {position && 
-        <small className="block pb-4">
-          {position}
+          {position && (
+            <h2 className="text-gray-500">
+              {position}
+              {companyName && (
+                <>
+                  {' at '}
+                  <span className="font-semibold">{companyName}</span>
+                </>
+              )}
+            </h2>
+          )}
+        </div>
+      </div>
 
-          {companyName && !companyUrl && <>{' at '}<strong>{companyName}</strong></>}
-          {companyName && companyUrl && <>{' at '}<a href={companyUrl}>{companyName}</a></>}
-        </small>
+
+      <article className="mb-4">
+        {content && content.map((line, index) => <p key={index}>{line}</p>)}
+      </article>
+
+      {socials &&
+        <div className="flex">
+          <ul className="inline-flex space-x-4">
+            {socials.map(links => (
+              <li key={links.url} className="flex-1 text-gray-500 xhover:text-gray-900">
+                <a
+                  role="button"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  href={links.url ?? '#'}
+                  className="text-gray-500"
+                  aria-label={links.handle ?? 'Social Media Link'}
+                >
+                  {mounted && <Icon name={links.icon} theme={theme} />}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       }
-
-      {content && content.map((line, index)=> <p key={index}>{line}</p>)}
-    </article>
+    </div>
   )
 }
